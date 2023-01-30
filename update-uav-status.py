d14df929ef7c93afe5f62d76f3cee8db44d4a89d
xoss <s.darmonski@uvionix.com>
2023-01-30 18:09:19 +0200

    Fixed initialization bug - vehicle armed status is now initially read from file

--- update-uav-status.py
+++ update-uav-status.py
@@ -10,10 +10,23 @@ AUTOPILOT_ARDUPILOT = 3
 VEHICLE_TYPE_QUAD = 2
 
 # Device initialization
-prev_armed="DISARMED"
-f = open("/etc/default/xoss-armed-status", "w")
-f.write(prev_armed+"\n")
-f.close()
+try:
+	# Get the armed status from the armed status file if it exists
+	f = open("/etc/default/xoss-armed-status", "r")
+	status_line = f.readline()
+	f.close()
+
+	prev_armed = status_line.splitlines()[0]
+
+	if prev_armed != "ARMED" and prev_armed != "DISARMED":
+		raise ValueError
+except (FileNotFoundError, IndexError, ValueError):
+	# Armed status file does not exist or its contents is not valid - create it with valid contents
+	prev_armed="DISARMED"
+	f = open("/etc/default/xoss-armed-status", "w")
+	f.write(prev_armed+"\n")
+	f.close()
+
 local_port = sys.argv[1]
 device_baudrate = int(sys.argv[2])
 device = 'udpin:localhost:'+local_port # Create a connection listening to a local UDP port
